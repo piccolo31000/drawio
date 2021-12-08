e32858401d170948c734188bc8eae477e3a162ba
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2021-12-08 23:17:55 +0100

    change function to rendering automaticaly after set Style props (#16)

--- src/main/webapp/js/diagramly/EditorUi.js
+++ src/main/webapp/js/diagramly/EditorUi.js
@@ -15052,7 +15052,7 @@
 								cellStyle = cellStyle.replace(search, `${key}=${value};`);
 							}
 						}
-						cell.setStyle(cellStyle);
+						graph.getModel().setStyle(cell, cellStyle);
 						return;
 					}
 					else if (data.action === 'setDataProperties') {
