be2a7f7d216f148e771e0a439b89520dbad7f8bc
Yassir-BenBOUBKER <97686091+Yassir-BenBOUBKER@users.noreply.github.com>
2023-06-05 12:58:07 +0100

    fix font Color selection (#98)

--- src/main/webapp/js/grapheditor/Format.js
+++ src/main/webapp/js/grapheditor/Format.js
@@ -1128,7 +1128,7 @@ BaseFormatPanel.prototype.createCellColorOption = function(label, colorKey, defa
 	{
 		// Seems to be null sometimes, not sure why...
 		var state = graph.view.getState(ui.getSelectionState().cells[0]);
-		
+  
 		if (state != null)
 		{
 			return mxUtils.getValue(state.style, colorKey, null);
@@ -3382,112 +3382,10 @@ TextFormatPanel.prototype.addFont = function(container)
 		graph.stylesheet.getDefaultVertexStyle() :
 		graph.stylesheet.getDefaultEdgeStyle();
 
-	var panel = (graph.cellEditor.isContentEditing()) ? this.createColorOption(mxResources.get('fontColor'), function()
-	{
-		return currentFontColor;
-	}, function(color)
-	{
-		if (mxClient.IS_FF)
-		{
-			// Workaround for Firefox that adds the font element around
-			// anchor elements which ignore inherited colors is to move
-			// the font element inside anchor elements
-			var tmp = graph.cellEditor.textarea.getElementsByTagName('font');
-			var oldFonts = [];
 
-			for (var i = 0; i < tmp.length; i++)
-			{
-				oldFonts.push(
-				{
-					node: tmp[i],
-					color: tmp[i].getAttribute('color')
-				});
-			}
-
-			document.execCommand('forecolor', false, (color != mxConstants.NONE) ?
-				color : 'transparent');
-			ui.fireEvent(new mxEventObject('styleChanged',
-				'keys', [mxConstants.STYLE_FONTCOLOR],
-				'values', [color], 'cells', ss.cells));
-
-			// Finds the new or changed font element
-			var newFonts = graph.cellEditor.textarea.getElementsByTagName('font');
-
-			for (var i = 0; i < newFonts.length; i++)
-			{
-				if (i >= oldFonts.length || newFonts[i] != oldFonts[i].node ||
-					(newFonts[i] == oldFonts[i].node &&
-						newFonts[i].getAttribute('color') != oldFonts[i].color))
-				{
-					var child = newFonts[i].firstChild;
-
-					// Moves the font element to inside the anchor element and adopts all children
-					if (child != null && child.nodeName == 'A' && child.nextSibling ==
-						null &&
-						child.firstChild != null)
-					{
-						var parent = newFonts[i].parentNode;
-						parent.insertBefore(child, newFonts[i]);
-						var tmp = child.firstChild;
-
-						while (tmp != null)
-						{
-							var next = tmp.nextSibling;
-							newFonts[i].appendChild(tmp);
-							tmp = next;
-						}
-
-						child.appendChild(newFonts[i]);
-					}
-
-					break;
-				}
-			}
-		}
-		else
-		{
-			document.execCommand('forecolor', false, (color != mxConstants.NONE) ?
-				color : 'transparent');
-			ui.fireEvent(new mxEventObject('styleChanged',
-				'keys', [mxConstants.STYLE_FONTCOLOR],
-				'values', [color], 'cells', ss.cells));
-		}
-	}, (defs[mxConstants.STYLE_FONTCOLOR] != null) ? defs[mxConstants.STYLE_FONTCOLOR] : graph.shapeForegroundColor,
-	{
-		install: function(apply) { fontColorApply = apply; },
-		destroy: function() { fontColorApply = null; }
-	}, null, true) : this.createCellColorOption(mxResources.get('fontColor'),
-		mxConstants.STYLE_FONTCOLOR, 'default', function(color)
-	{
-		if (color == mxConstants.NONE)
-		{
-			bgPanel.style.display = 'none';
-		}
-		else
-		{
-			bgPanel.style.display = '';
-		}
-		
-		borderPanel.style.display = bgPanel.style.display;
-	}, function(color)
-	{
-		if (color == mxConstants.NONE)
-		{
-			graph.setCellStyles(mxConstants.STYLE_NOLABEL, '1', ss.cells);
-		}
-		else
-		{
-			graph.setCellStyles(mxConstants.STYLE_NOLABEL, null, ss.cells);
-		}
-		
-		graph.setCellStyles(mxConstants.STYLE_FONTCOLOR, color, ss.cells);
-
-		graph.updateLabelElements(ss.cells, function(elt)
-		{
-			elt.removeAttribute('color');
-			elt.style.color = null;
-		});
-	}, graph.shapeForegroundColor);
+	var panel = this.createCellColorOption(mxResources.get('fontColor'),
+	mxConstants.STYLE_FONTCOLOR, 'default', null, null, graph.getDefaultColor(ss.style, mxConstants.STYLE_FONTCOLOR, graph.shapeBackgroundColor,
+	graph.shapeForegroundColor));
 	panel.style.fontWeight = 'bold';
 	
 	colorPanel.appendChild(panel);
